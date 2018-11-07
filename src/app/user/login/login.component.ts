d25a2c8fce014726813a1791a952c51c3bcc575a
ajay-arya <ajayarya512@gmail.com>
2018-11-08 01:19:47 +0530

    half validation

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
+import { isRegExp } from 'util';
+import { and } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-login',
@@ -7,15 +9,31 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  Uname;
+  Uname: string;
   password;
   usernameValidation = false;
+  passwordValidation = false;
+  userValidation = false;
+  validUN;
+  validPS;
+  user: any = [{
+    uname: 'ajay-arya',
+    password: 'ajay123'
+  }, {
+    uname: 'abc',
+    password: 'ajay123'
+  }, {
+    uname: '1',
+    password: '2'
+  }];
+  constructor(private router: Router, public render: Renderer2, public el: ElementRef) { }
 
-  constructor(private router: Router, public render: Renderer2, public el:ElementRef) { }
+  ngOnInit() {
+    this.Uname = "";
+    this.password = '';
+  }
 
-  ngOnInit() {  }
-
-  signup()  {
+  signup() {
     const mov = this.el.nativeElement.getElementsByClassName('color')[0];
     this.render.addClass(mov, 'move');
     setTimeout(() => {
@@ -24,9 +42,59 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-    console.log(this.Uname);
-    console.log(this.password);
-    // this.router.navigate(['Chatbot']);
+    const mov = this.el.nativeElement.getElementsByClassName('color')[0];
+    const finish = this.el.nativeElement.getElementsByClassName('Acenter')[0];
+    var flag = 0;
+    if (this.Uname.length === 0) {
+      this.validUN = '*Enter a User Name';
+      this.usernameValidation = true;
+    } else if (this.password.length === 0) {
+      this.validPS = '*Enter a Password';
+      this.passwordValidation = true;
+    } else {
+      for (var i of this.user) {
+        // console.log(i);
+        if (i.uname === this.Uname)
+          if (i.password === this.password) {
+            flag = 1;
+          }
+      }
+      if (flag === 0) {
+        this.userValidation = true;
+      } else {
+        this.render.addClass(finish, 'dis');
+        this.render.addClass(mov, 'move');
+        setTimeout(() => {
+          this.render.addClass(mov, 'move-half');
+        }, 500);
+        setTimeout(() => {
+          this.router.navigate(['signup']);
+        }, 1250);
+      }
+      // this.router.navigate(['Chatbot']);
+    }
   }
 
+  validate(data, flag) {
+    if (data !== 'Enter') {
+      this.userValidation = false;
+    }
+
+    this.usernameValidation = false;
+    if (flag === 1) {
+      var pattern = /[A-Za-z0-9]$/;
+
+      if (data === ' ') {
+        this.validUN = '*No Spaces are allowed';
+        this.usernameValidation = !this.usernameValidation;
+        this.Uname = this.Uname.trim();
+      } else if (!pattern.test(data) && data !== '-') {
+        this.validUN = "*Special charecters are not allowed";
+        this.usernameValidation = !this.usernameValidation;
+        this.Uname = this.Uname.slice(0, -1);
+      }
+    } else if (flag === 2) {
+      this.passwordValidation = false;
+    }
+  }
 }
--- src/app/user/signup/signup.component.ts
+++ src/app/user/signup/signup.component.ts
@@ -7,21 +7,53 @@ import { Router } from '@angular/router';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  Fname = '';
+  Uname = '';
+  Email = '';
+  password = '';
+  Cpassword = '';
+  validFN = false;
+  validUN = false;
+  validEM = false;
+  validPS = false;
+  validCP = false;
+  mesFN;
+  mesUN;
+  mesEM;
+  mesPS;
+  mesCP;
+
+  validFill = false;
+  mes = ' *Fill all details';
 
   constructor(private router: Router, public el: ElementRef, public render: Renderer2) { }
 
   ngOnInit() {
   }
-  login()  {
+  login() {
     const mov = this.el.nativeElement.getElementsByClassName('color')[0];
     const cover = this.el.nativeElement.getElementsByClassName('Acenter')[0];
-    
+
     // this.render.addClass(cover, 'bk');
     this.render.removeClass(cover, 'dis');
     this.render.addClass(mov, 'move');
     setTimeout(() => {
       this.router.navigate(['login']);
     }, 500);
+  }
+
+  signup() {
+
+    if((this.Fname.length === 0) || (this.Uname.length === 0) || (this.Email.length === 0) || (this.password.length === 0)) {
+      this.validFill = true;
+    } else {
+      
+    }
+
+  }
+
+  validate(data, flag) {
+
   }
 
 }
